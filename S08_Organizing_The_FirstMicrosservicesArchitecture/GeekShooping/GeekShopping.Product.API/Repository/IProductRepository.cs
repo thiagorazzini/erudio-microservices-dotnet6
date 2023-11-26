e391bee801eba226526e71d979ddc45f22a8ebcd
Thiago Razzini <razzini.thiago@gmail.com>
2023-11-26 12:47:28 -0300

    Defindo as propriedades da interface IProductRepository

--- S08_Organizing_The_FirstMicrosservicesArchitecture/GeekShooping/GeekShopping.Product.API/Repository/IProductRepository.cs
+++ S08_Organizing_The_FirstMicrosservicesArchitecture/GeekShooping/GeekShopping.Product.API/Repository/IProductRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace GeekShopping.Product.API.Repository
+﻿using GeekShopping.Product.API.Data.ValueObjects;
+
+namespace GeekShopping.Product.API.Repository
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductVO>> FindAll();
+        Task<ProductVO> FindByID(long id);
+        Task<ProductVO> Create(ProductVO vo);
+        Task<ProductVO> Update(ProductVO vo);
+        Task<bool> Delete(long id);
     }
 }
